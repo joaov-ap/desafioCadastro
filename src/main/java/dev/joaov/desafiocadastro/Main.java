8b3a07c5b4aa60cc68a71137c355ec83ef671201
joaov-ap <vitor.1943@gmail.com>
2025-08-11 12:00:23 -0300

    feat: step 1 done, reading file 'formulario.txt' with questions

--- src/main/java/dev/joaov/desafiocadastro/Main.java
+++ src/main/java/dev/joaov/desafiocadastro/Main.java
@@ -1,7 +1,28 @@
 package main.java.dev.joaov.desafiocadastro;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Inicio");
+        List<String> questions = new ArrayList<>();
+
+        final String FILE_PATH = "src/main/resources/formulario.txt" ;
+        File file = new File(FILE_PATH);
+
+        try (FileReader fileReader = new FileReader(file);
+             BufferedReader reader = new BufferedReader(fileReader)) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                questions.add(line);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        questions.forEach(System.out::println);
     }
 }
